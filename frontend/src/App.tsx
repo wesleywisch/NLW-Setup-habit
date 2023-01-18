c4eb886ca7ac684a78d244c6e5b8c1943cbe12e9
wesley_wisch <wesley_wisch@hotmail.com>
2023-01-18 16:55:57 -0300

    feat: frontend setup completed.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Habit } from "./components/Habit"
+
+function App() {
+  return (
+    <div>
+      <Habit completed={3} />
+      <Habit completed={2} />
+    </div>
+  )
+}
+
+export default App
